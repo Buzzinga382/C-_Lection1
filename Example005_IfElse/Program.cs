5815a7a9ff1f41c90b71f008d33387fe14b857f3
Anthony <gugl382@gmail.com>
2022-11-20 15:08:09 +0300

    Added Example005

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please enter Username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "anthony")
+{
+    Console.WriteLine("Hey, wazzup Anthony");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
